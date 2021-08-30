d20f00be5821e571cb7643d404ca00b1bfa14da7
Andy <alopezciotta@gmail.com>
2021-08-29 23:13:27 -0300

    agrego precio por apps y total con innerHTML

---
+++ script.js
@@ -0,0 +1,16 @@
+function selectorRange () {
+    const a = document.getElementById('options').value
+    return console.log(a)
+};
+
+
+let rangeMovieValue = document.getElementById('button').addEventListener('click', selectorRange )
+
+
+
+
+
+
+
+
+
